16f89bc42ce00b5adaa8755d445b428bb2811ad9
priitpaap <44830260+priitpaap@users.noreply.github.com>
2025-09-10 18:50:42 +0300

    Update yl5.2-setup.sh

--- yl5.2-setup.sh
+++ yl5.2-setup.sh
@@ -14,21 +14,23 @@ if ! id -u student >/dev/null 2>&1; then
 fi
 
 # Eemalda paketid, mis võivad olla eelnevalt paigaldatud
-apt purge -y nginx mc fortune-mod >/dev/null 2>&1 || true
-dpkg -r webmin >/dev/null 2>&1 || true
+sudo dnf remove -y nginx mc fortune-mod >/dev/null 2>&1 || true
+sudo rpm -e webmin >/dev/null 2>&1 || true
 
-# Puhasta
-apt autoremove -y >/dev/null 2>&1 || true
-apt clean >/dev/null 2>&1 || true
+# Puhasta süsteem
+sudo dnf autoremove -y >/dev/null 2>&1 || true
+sudo dnf clean all >/dev/null 2>&1 || true
+
 
 # Paigalda bind9
 dnf install -y samba >/dev/null 2>&1 || true
 
 # Kustuta failid ja käsuajalugu
-su - student -c "rm -f ${STUDENT_HOME}/debian.txt ${STUDENT_HOME}/nginx.txt ${STUDENT_HOME}/depends.txt" >/dev/null 2>&1
+su - student -c "rm -f ${STUDENT_HOME}/alma.txt ${STUDENT_HOME}/httpd.txt ${STUDENT_HOME}/depends.txt" >/dev/null 2>&1
 su - student -c "history -c" >/dev/null 2>&1
 
 # Lae alla Webmini installifail (wget väljastaks muidu infot)
-su - student -c "wget -q -O ${STUDENT_HOME}/webmin_2.402_all.deb https://sourceforge.net/projects/webadmin/files/webmin/2.402/webmin_2.402_all.deb/download" >/dev/null 2>&1
+su - student -c "wget -q -O ${STUDENT_HOME}/sl-5.02-1.el9.x86_64.rpm wget https://dl.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/s/sl-5.02-1.el9.x86_64.rpm
+" >/dev/null 2>&1
 
 echo ">>> Ettevalmistus tehtud! Saad nüüd ülesandega alustada."
